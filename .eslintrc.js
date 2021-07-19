a624ddc930c31beb75c7083dedd4129cd4f3116c
liusx@zhulong.com.cn <liusx@zhulong.com.cn>
2021-07-19 18:02:27 +0800

    feat: 添加sudoku client & server

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,8 @@ module.exports = {
   extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
   parserOptions: {
     parser: 'babel-eslint',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module'
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
