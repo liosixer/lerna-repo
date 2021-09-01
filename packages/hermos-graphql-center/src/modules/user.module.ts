17b6e71b199720a3de0da27d346cc441f7d59e54
刘绍雄 <liusx@zhulong.com.cn>
2021-09-01 17:48:43 +0800

    update: projects update

--- packages/hermos-graphql-center/src/modules/user.module.ts
+++ packages/hermos-graphql-center/src/modules/user.module.ts
@@ -3,9 +3,17 @@ https://docs.nestjs.com/modules
 */
 
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-    imports: [],
+    imports: [
+      GraphQLModule.forRoot({
+        debug: false,
+        include: [
+
+        ]
+      }),
+    ],
     controllers: [],
     providers: [],
 })
